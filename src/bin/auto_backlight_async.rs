45448cacaa6c656875c2ef8da4d4928189751580
Tao Tien <29749622+taotien@users.noreply.github.com>
2023-03-23 00:19:53 -0700

    rewrite logic for async backlight

--- src/bin/auto_backlight_async.rs
+++ src/bin/auto_backlight_async.rs
@@ -3,136 +3,174 @@ use splines::{Interpolation, Key, Spline};
 use tokio::{
     fs::{read_to_string, write},
     join, spawn,
-    sync::Mutex,
     task::JoinHandle,
     time::sleep,
 };
 
-use std::{collections::VecDeque, sync::Arc, time::Duration};
+use std::{
+    collections::VecDeque,
+    sync::{atomic::AtomicU32, atomic::Ordering, Arc},
+    time::Duration,
+};
 
-const SAMPLE_SIZE: usize = 10;
-const SAMPLE_INTERVAL_MS: u64 = 1000;
+const SAMPLE_SIZE: u32 = 100;
+const SAMPLE_INTERVAL_MS: u64 = 100;
 const FPS: u32 = 60;
-const HISTERESIS: i32 = 5000;
+const TPF: u64 = 1000 / FPS as u64;
+// const HISTERESIS: u32 = 5000;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     #[cfg(tokio_unstable)]
     console_subscriber::init();
 
-    let b = Arc::new(Mutex::new(Backlight::new().await?));
+    let mut backlight = Backlight::new().await.unwrap();
+    let mut sensor = Sensor::new().await.unwrap();
+    let average = Arc::new(AtomicU32::new(Sensor::get().await.unwrap()));
+    // let (sw, mut sr) = watch::channel(sensor.average);
 
-    let a = Arc::clone(&b);
-    let adjust: JoinHandle<Result<()>> = spawn(async move {
-        loop {
-            sleep(Duration::from_millis(1000 / FPS as u64)).await;
-            let mut a = a.lock().await;
-            if !a.changed().await? {
-                a.adjust().await?;
-            }
-        }
-    });
-
-    let s = Arc::clone(&b);
+    let avg = average.clone();
     let sample: JoinHandle<Result<()>> = spawn(async move {
         loop {
+            sensor.sample().await.unwrap();
+            avg.store(
+                sensor.samples.iter().sum::<u32>() / SAMPLE_SIZE,
+                Ordering::Relaxed,
+            );
             sleep(Duration::from_millis(SAMPLE_INTERVAL_MS)).await;
-            let mut s = s.lock().await;
-            s.sample().await?;
+            // println!("sample");
         }
     });
 
-    let r = Arc::clone(&b);
-    let retain: JoinHandle<Result<()>> = spawn(async move {
+    // let s = Arc::clone(&sensor);
+    let avg = average.clone();
+    let adjust_retain: JoinHandle<Result<()>> = spawn(async move {
         loop {
-            sleep(Duration::from_millis(SAMPLE_INTERVAL_MS)).await;
-            let mut r = r.lock().await;
-            if r.changed().await? {
-                r.retain().await?;
+            if !backlight.changed().await.unwrap() {
+                let d;
+                if Backlight::get().await.unwrap() != backlight.target {
+                    backlight.adjust().await.unwrap();
+                    d = Duration::from_millis(TPF);
+                    // println!(
+                    //     "{} {} {}",
+                    //     Backlight::get().unwrap(),
+                    //     backlight.diff, backlight.target
+                    // );
+                } else {
+                    d = Duration::from_millis(SAMPLE_INTERVAL_MS * 10);
+                }
+                backlight
+                    .prepare(avg.load(Ordering::Relaxed))
+                    .await
+                    .unwrap();
+                sleep(d).await;
+                // println!("prepare");
+            } else {
+                backlight.retain(avg.load(Ordering::Relaxed)).await.unwrap();
+                // println!("retain");
             }
         }
     });
 
-    let _ = join![retain, sample, adjust];
+    let _ = join![sample, adjust_retain];
     unreachable!()
 }
 
-#[derive(Debug)]
+struct Sensor {
+    samples: VecDeque<u32>,
+}
+
+impl Sensor {
+    async fn sample(&mut self) -> Result<()> {
+        self.samples.pop_front();
+        self.samples.push_back(Self::get().await.unwrap());
+        Ok(())
+    }
+
+    async fn get() -> Result<u32> {
+        Ok(
+            read_to_string("/sys/bus/iio/devices/iio:device0/in_illuminance_raw")
+                .await
+                .unwrap()
+                .trim()
+                .parse()
+                .unwrap(),
+        )
+    }
+    async fn new() -> Result<Self> {
+        let samples = VecDeque::from([Self::get().await.unwrap(); SAMPLE_SIZE as usize]);
+        Ok(Self { samples })
+    }
+}
+
 struct Backlight {
     requested: u32,
-    samples: VecDeque<u32>,
-    sample_map: Vec<u32>,
-    curve: Spline<f32, f32>,
+    target: u32,
     diff: i32,
+    step: i32,
+    curve: Spline<f32, f32>,
+    // curve_map:
 }
 
 impl Backlight {
+    async fn prepare(&mut self, s: u32) -> Result<()> {
+        self.target = self.curve.clamped_sample(s as f32).unwrap() as u32;
+        self.diff = self.target as i32 - Self::get().await.unwrap() as i32;
+        self.step = self.diff / FPS as i32;
+        Ok(())
+    }
+
     async fn adjust(&mut self) -> Result<()> {
-        if self.diff != 0 {
-            let mut step = self.diff / FPS as i32;
-            if step == 0 {
-                step = if self.diff > 0 { 1 } else { -1 };
-            }
-            let v = Self::get().await? as i32 + step;
-
-            self.set(v as u32).await?;
+        self.diff = self.target as i32 - Self::get().await.unwrap() as i32;
+        if self.step == 0 {
+            self.step = if self.diff > 0 { 1 } else { -1 }
         }
+        let v = Self::get().await.unwrap() as i32 + self.step;
+        if v < 0 {
+            return Ok(());
+        }
+        println!("{} {}", self.step, v);
+        self.set(v as u32).await.unwrap();
         Ok(())
     }
 
-    async fn sample(&mut self) -> Result<()> {
-        self.samples.pop_front();
-        self.samples.push_back(Self::sensor().await?);
-        let target = self.curve.clamped_sample(self.average() as f32).unwrap();
-        // let target = self.sample_map[self.average() as usize];
-        self.diff = target as i32 - Self::get().await? as i32;
-
-        Ok(())
-    }
-
-    async fn retain(&mut self) -> Result<()> {
+    async fn retain(&mut self, s: u32) -> Result<()> {
         sleep(Duration::from_secs(5)).await;
-        let current = Self::get().await?;
+        let current = Self::get().await.unwrap();
 
         if current == 0 {
             return Ok(());
         }
 
         self.requested = current;
-        self.curve
-            .monotonic_add(self.average() as f32, current as f32);
-
-        // for i in 0..self.curve.len() {
-        //     self.sample_map[i] = self.curve.clamped_sample(i as f32).unwrap() as u32;
-        // }
-
+        self.curve.monotonic_add(s as f32, current as f32);
+        self.prepare(s).await.unwrap();
         Ok(())
+    }
+
+    async fn changed(&self) -> Result<bool> {
+        Ok(Self::get().await.unwrap() as i32 - self.requested as i32 != 0)
     }
 
     async fn get() -> Result<u32> {
         Ok(
             read_to_string("/sys/class/backlight/intel_backlight/brightness")
-                .await?
+                .await
+                .unwrap()
                 .trim()
-                .parse()?,
-        )
-    }
-
-    async fn sensor() -> Result<u32> {
-        Ok(
-            read_to_string("/sys/bus/iio/devices/iio:device0/in_illuminance_raw")
-                .await?
-                .trim()
-                .parse()?,
+                .parse()
+                .unwrap(),
         )
     }
 
     async fn set(&mut self, val: u32) -> Result<()> {
+        // println!("{}", val);
         write(
             "/sys/class/backlight/intel_backlight/brightness",
             val.to_string(),
         )
-        .await?;
+        .await
+        .unwrap();
         self.requested = val;
         Ok(())
     }
@@ -140,38 +178,32 @@ impl Backlight {
     async fn max() -> Result<u32> {
         Ok(
             read_to_string("/sys/class/backlight/intel_backlight/max_brightness")
-                .await?
+                .await
+                .unwrap()
                 .trim()
-                .parse()?,
+                .parse()
+                .unwrap(),
         )
     }
-
-    fn average(&self) -> u32 {
-        self.samples.iter().sum::<u32>() / self.samples.len() as u32
-    }
-
-    async fn changed(&self) -> Result<bool> {
-        let diff: i32 = Self::get().await? as i32 - self.requested as i32;
-        Ok(diff != 0)
-    }
-
     async fn new() -> Result<Self> {
-        let requested = Self::get().await?;
-        let samples = VecDeque::from([Self::sensor().await?; SAMPLE_SIZE]);
-        let floor = Key::new(0., 1., Interpolation::default());
-        let ceil = Key::new(3355., Self::max().await? as f32, Interpolation::default());
-        let curve = Spline::from_vec(vec![floor, ceil]);
-        let mut sample_map = Vec::with_capacity(3355 + 1);
-        // for i in 0..curve.len() {
-        //     sample_map.push(curve.clamped_sample(i as f32).unwrap() as u32);
-        // }
+        let current = Self::get().await.unwrap();
+        let requested = current;
+        let target = current;
         let diff = 0;
+        let step = 0;
+        let floor = Key::new(0., 1., Interpolation::default());
+        let ceil = Key::new(
+            3355.,
+            Self::max().await.unwrap() as f32,
+            Interpolation::default(),
+        );
+        let curve = Spline::from_vec(vec![floor, ceil]);
         Ok(Self {
             requested,
-            samples,
-            sample_map,
-            curve,
+            target,
             diff,
+            step,
+            curve,
         })
     }
 }
