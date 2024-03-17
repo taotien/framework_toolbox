42b0f30ce15169ea6477f26c40423b334a63810e
Tao Tien <29749622+taotien@users.noreply.github.com>
2024-03-17 00:35:40 -0700

    flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem
+    (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in
+      with pkgs; rec {
+        devShell = mkShell rec {
+          buildInputs = [
+            libxkgcommon
+            libGL
+
+            wayland
+
+            xorg.libXcursor
+            xorg.libXrandr
+            xorg.libXi
+            xorg.libX11
+          ];
+          LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+        };
+      });
+}
